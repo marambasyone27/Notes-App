190649903ca9dd0bc19fa0aad7ae4b85d2d5723c
marambasyone27 <marambasyone1@gmail.com>
2025-08-24 18:14:40 +0300

    create add note cubit

---
+++ lib/cubits/add_noteCubit/notes_states.dart
@@ -0,0 +1,11 @@
+part of 'notes_cubit.dart';
+
+abstract class NotesState{}
+
+class NotesInitialState extends NotesState{}
+class NotesLoadingState extends NotesState{}
+class NotesSuccessState extends NotesState{}
+class NotesFailureState extends NotesState{
+  final String errorMessage;
+  NotesFailureState({required this.errorMessage});
+}
